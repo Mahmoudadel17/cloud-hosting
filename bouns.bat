c7339cab5bfac1cc7a6255d2c46ef424fa2d148c
Mahmoud Adel <mahmoudadel5556@gmail.com>
2024-05-18 11:00:33 +0300

    commit 4

--- bouns.bat
+++ bouns.bat
@@ -3,7 +3,7 @@ REM Navigate to a working directory
 cd /D C:\Users\PC\Desktop\CloudTask\cloud-hosting
 
 REM Clone the repository
-https://github.com/Mahmoudadel17/cloud-hosting2.git
+git clone https://github.com/Mahmoudadel17/cloud-hosting2.git
 
 REM Navigate to the cloned repository
 cd cloud-hosting2
